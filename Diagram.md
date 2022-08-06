09afe5927c3f9b7bc71483fd9a2f87d34582f71f
lumosmax7 <lmyleo7@gmail.com>
2022-08-06 16:41:23 +0800

    finish init,config and commit.

--- Diagram.md
+++ Diagram.md
@@ -10,3 +10,12 @@
 	* 如果有`.git`，则根据git来
 	* 如果没有则初始化生成
 * 追踪Git，实现Git的功能
+
+## 代码逻辑0806
+修改逻辑到先考虑本地化功能，在考虑远端仓库;
+* gitBashDev init ; 初始化git，并且生成本地git仓库,
+* gitBashDev config; 配置远程github仓库设置
+* gitBashDev commit；执行diff，查看区别；执行commit指令
+* gitBashDev 执行push到github上；
+
+
--- GitDev.sh
+++ GitDev.sh
@@ -4,117 +4,180 @@ currentGitEmail=$(git config user.email)
 currentGitName=$(git config user.name) #no blank space
 
 
-
-echo Welcome to the Git-command-line tool
-echo ------------------------------------
-echo Check Git installed;
-echo ------------------------------------
-
-if  [ ! "$(command -v git)" ];
-then
-    echo Git is not installed.
-    echo ---------------------
-    echo Do you want to install Git?
-    echo 1.yes
-    echo 2.no
-    echo \(default: yes\)
-    read installGit #here is a string, not a number
-    if [ -z $installGit ] || [ $installGit == "1" ];
+initProcess(){
+    echo Check Git installed;
+    echo ------------------------------------
+    if  [ ! "$(command -v git)" ];
     then
-        echo installing Git
-        # install command
-        if [ $currentOS == "Darwin" ];
+        echo Git is not installed.
+        echo ---------------------
+        echo Do you want to install Git?
+        echo 1.yes
+        echo 2.no
+        echo \(default: yes\)
+        read installGit #here is a string, not a number
+        if [ -z $installGit ] || [ $installGit == "1" ];
         then
-            brew install git
-        elif [ $currentOS == "Debian"  ];
-        then
-            apt-get install git
-        elif [ $currentOS == "CentOS" ];
-        then
-            yum instsall git
+            echo installing Git
+            # install command
+            if [ $currentOS == "Darwin" ];
+            then
+                brew install git
+            elif [ $currentOS == "Debian"  ];
+            then
+                apt-get install git
+            elif [ $currentOS == "CentOS" ];
+            then
+                yum instsall git
+            fi
+        else
+            echo This tool needs Git, be sure to install Git manually.
+            exit
         fi
+        echo ------------------------------------
+        
     else
-        echo This tool needs Git, be sure to install Git manually.
-        exit
+        echo git is installed
+        echo ------------------------------------
     fi
-    echo ------------------------------------
-
-else
-    echo git is installed
-    echo ------------------------------------
-fi
-
-# check account setted
-# if not set account
-
-# echo Checking account
-# echo ------------------------------------
-# if [ ! -z $currentGitEmail ] && [ ! -z $currentGitName ];
-# then
-#     echo Account is $currentGitName
-#     echo Email is $currentGitEmail
-#     echo ------------------------------------
-# else
-#     echo Setting account
-#     while true
-#     do
-#         read -p "Your email: " email
-#         read -p "Your name: " name
-#         if [ ! -z $email ] && [ ! -z $name ];
-#         then
-#             break
-#         fi
-#     done
-#     git config --global user.name $name
-#     git config --global user.email $email
-#     echo ------------------------------------
-# fi
-
-# echo Finish initiate account
-# echo ------------------------------------
-
-while true
-do
-    echo Choosing working directory: \(defalut: current directory\)
-    read workingDir
-    if [ -z $workingDir ];
-    then
-        workingDir=$(pwd);
-    fi
-    if [ -d $workingDir ];
-    then
-        break;
-    fi
-    echo Wrong directory.
     
-done 
+    while true
+    do
+        echo Choosing working directory: \(defalut: current directory\)
+        read workingDir
+        if [ -z $workingDir ];
+        then
+            workingDir=$(pwd);
+        fi
+        if [ -d $workingDir ];
+        then
+            break;
+        fi
+        echo Wrong directory.
+        
+    done
     echo Working directory is $workingDir
     echo ------------------------------------
     cd $workingDir
+    
+    
+    if [ -d $workingDir/.git ];
+    then
+        echo Current directory has deployed Git.
+        echo ------------------------------------
+        echo Show git status.
+        echo ------------------------------------
+        git status
+        echo ------------------------------------
+        echo List tracking files.
+        echo ------------------------------------
+        git ls-files
+        
+    else
+        echo ------------------------------------
+        echo Init a git repository
+        echo ------------------------------------
+        git init
+        git add -A
+    fi
+    echo ------------------------------------
+    echo Finish deploying Git on $workingDir
+    echo ------------------------------------
+    
+    
+}
 
 
-if [ -d $workingDir/.git ];
-then 
-    echo  Current directory has deployed Git.
+configProcess(){
+    echo Checking account
     echo ------------------------------------
-    echo Show git status.
+    if [ ! -z $currentGitEmail ] && [ ! -z $currentGitName ];
+    then
+        echo Account is $currentGitName
+        echo Email is $currentGitEmail
+        echo ------------------------------------
+    else
+        echo Setting account
+        while true
+        do
+            read -p "Your email: " email
+            read -p "Your name: " name
+            if [ ! -z $email ] && [ ! -z $name ];
+            then
+                break
+            fi
+        done
+        git config --global user.name $name
+        git config --global user.email $email
+        echo ------------------------------------
+    fi
+    
+    echo Check connection with Github. Please wait and according to the result to install ssh key manually or not.
+    ssh -T git@github.com
+    echo Do you need to config ssh with Github? \(yes or no, defalut is no\)
+    read configFlag
+    if [ ! $configFlag ];
+    then
+        configFlag="no"
+    fi
+    if [ $configFlag == "yes" ];
+    then
+        # ssh-keygen -t ed25519 -C $currentGitEmail
+        echo ------------------------------------
+        echo Please copy the private key and paste on Git.com
+        echo ------------------------------------
+        cat ~/.ssh/id_rsa
+    fi
     echo ------------------------------------
-    git status
+    echo Finish initiate account
     echo ------------------------------------
-    echo List tracking files.
-    echo ------------------------------------
-    git ls-files
+    
+}
 
-else
-    echo ------------------------------------
-    echo Init a git repository
-    echo ------------------------------------
-    git init
+commitProcess(){
+    echo Do you need to check the differences \(yes or no, defalut is no\)?
+    read checkFlag
+    if [ ! $checkFlag ];
+    then
+        checkFlag="no"
+    fi
+    if [ "$checkFlag" == "yes" ];
+    then
+        echo check git status 
+        echo ------------------------------------
+        git diff
+    fi
+    echo "Enter the commiting message? (default is timestamp)"
+    read message
+    echo "$message"
+    if [ ! "$message" ];
+    then
+        local timeStamp=$(date +%F-%T)
+        git add -A
+        git commit -m "$timeStamp"
+    fi
     git add -A
-fi
-echo ------------------------------------
-echo Finish deploying Git on $workingDir
-echo ------------------------------------
+    git commit -m "$message"
+}
 
 
-#TODO: add "commit" and "diff" command function.
+echo Welcome to the Git-command-line tool.
+echo bash GitDev.sh help for more.
+echo ------------------------------------
+
+case $1 in
+    "init")
+    initProcess;;
+    "config")
+    configProcess;;
+    "commit")
+    commitProcess;;
+    "help")
+        echo usage: bash GitDev.sh init \| config \| commit \| push \| help
+        echo ------------------------------------
+        echo init: init a repository in the current directory.
+        echo config: config a account used to connect to Github.
+        echo commit: git diff and git commit.
+        echo push: push to Github.
+    ;;
+esac
